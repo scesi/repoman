78dcd433fc77423bf840b06b1183eacfa8bbd061
root <jorge.encinas@gmail.com>
2017-08-13 01:08:32 -0400

    add: elementary and trisquel

--- index.php
+++ index.php
@@ -36,7 +36,9 @@
             'slackware'  => array('title' => 'Slackware'),
             'ubuntu'     => array('title' => 'Ubuntu'),
             'uremix'     => array('title' => 'Uremix'),
-            'manjaro'    => array('title' => 'Manjaro'),
+            'manjaro'    => array('title' => 'Manjaro Linux'),
+	    'trisquel'   => array('title' => 'Trisquel GNU/Linux'),
+	    'elementary' => array('title' => 'elementary OS'),
         );
 
         foreach ($directories as $directory) {
